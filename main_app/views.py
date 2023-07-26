7b6be8235a7c5890554b765c6eda8736f3eb4ee5
Rami Bankale <ramibankale@Ramis-MBP.attlocal.net>
2023-07-26 16:30:39 -0500

    import Art Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
+from .models import Art
+
 
 # Add this arts list below the imports
-arts = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+
 
 # Create your views here.
 # Define the home view
@@ -16,6 +15,7 @@ def about(request):
   return render(request, 'about.html')
 
 def arts_index(request):
+  arts = Art.objects.all() # Retrieve all cats
   # We pass data to a template very much like we did in Express!
   return render(request, 'arts/index.html', {
     'arts': arts
